6ae8ff80c27639651bd43fd20028c64e78549d83
dobrapyra <zielinski00@gmail.com>
2024-01-27 18:29:25 +0100

    add bazier easing

--- src/js/easing.js
+++ src/js/easing.js
@@ -52,6 +52,39 @@ export function easeInOutQuart(f) {
   return 1 - ((f * f * f * f) / 2);
 }
 
+export function cubicBezierAxis(p1, c1, c2, p2, f) {
+  const nf = 1 - f;
+
+  return (
+    nf * nf * nf * p1
+    + 3 * nf * nf * f * c1
+    + 3 * nf * f * f * c2
+    + f * f * f * p2
+  );
+}
+
+export function cubicBezier(p1, c1, c2, p2, f) {
+  return [
+    cubicBezierAxis(p1[0], c1[0], c2[0], p2[0], f),
+    cubicBezierAxis(p1[1], c1[1], c2[1], p2[1], f),
+  ];
+}
+
+/**
+ *
+ * @param {number} c1 control point 1 x
+ * @param {number} c2 control point 2 x
+ * @returns {function}
+ */
+export function getEaseBazier(c1, c2) {
+  const p1 = 0;
+  const p2 = 1;
+
+  return function easeBazier(f) {
+    return cubicBezierAxis(p1, c1, c2, p2, f);
+  };
+}
+
 export default {
   linear,
   easeInQuad,
@@ -63,4 +96,5 @@ export default {
   easeInQuart,
   easeOutQuart,
   easeInOutQuart,
+  getEaseBazier,
 };
